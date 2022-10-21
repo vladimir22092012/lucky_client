174e7fe9fce7b2f4402a531391277a23ece67d54
Андрей Попов <duircianos@yandex.ru>
2022-10-21 10:34:22 +0400

    открыл возможность брать займ

--- ajax/check_phone.php
+++ ajax/check_phone.php
@@ -8,61 +8,60 @@ require 'autoload.php';
 class CheckPhone extends Core
 {
     private $response = array();
-    
+
     public function __construct()
     {
-    	parent::__construct();
-        
+        parent::__construct();
+
     }
-    
-    
+
+
     public function run()
     {
         $phone = $this->request->get('phone');
-        
+
         $clear_phone = $this->sms->clear_phone($phone);
-        
+
+        $user_id = $this->users->get_phone_user($clear_phone);
+
+        $user = $this->users->get_user($user_id);
+
         if (strlen($clear_phone) != 11)
         {
             $this->response['incorrect'] = $clear_phone;
         }
         else
         {
-            $exist_id = $this->users->get_phone_user($clear_phone);
+            if ($user->blocked == 1) {
+                $this->response['user_removed'] = 1;
+                $this->output();
+            }
 
-            if (!empty($exist_id))
+            if ($exist_id = $this->users->get_phone_user($clear_phone))
             {
-                $user = $this->users->get_user((int)$exist_id);
+                $this->response['user_exists'] = 1;
 
+                $user = $this->users->get_user((int)$exist_id);
                 if (empty($this->is_developer))
                     $this->response['have_pass'] = (int)!empty($user->password);
-
-                if (!empty($user->removed))
-                    $this->response['user_removed'] = 1;
-
-                $this->response['user_exists'] = 1;
+            }
+            else
+            {
+                $this->response['not_found'] = 1;
             }
 
-            if (!empty($user->removed))
-                $this->response['user_removed'] = 1;
-            
-            if (!empty($user) && $user->blocked) {
-                $this->response['user_removed'] = 1;
-            }
-
-            
         }
-        
+
         $this->output();
     }
-    
+
     private function output()
     {
-       	header("Content-type: application/json; charset=UTF-8");
+        header("Content-type: application/json; charset=UTF-8");
         header("Cache-Control: must-revalidate");
         header("Pragma: no-cache");
-        header("Expires: -1");	
-        
+        header("Expires: -1");
+
         echo json_encode($this->response);
         exit;
 
@@ -70,4 +69,4 @@ class CheckPhone extends Core
 }
 
 $check_phone = new CheckPhone();
-$check_phone->run(); 
+$check_phone->run();
