e25ad3896c799f6c45b300bb4a15a617a5fbb344
Андрей Попов <duircianos@yandex.ru>
2022-11-01 10:38:58 +0400

    индивидуальные условия

--- controllers/DocumentController.php
+++ controllers/DocumentController.php
@@ -1,72 +1,99 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'Off');
+ini_set('max_execution_time', 120);
+
 class DocumentController extends Controller
 {
     public function fetch()
     {
 //echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($document);echo '</pre><hr />';        
-        if ($this->request->get('action') == 'preview')
-        {
+        if ($this->request->get('action') == 'preview') {
             $this->action_preview();
         }
-        
+
         $id = $this->request->get('id');
         $id = str_replace('.pdf', '', $id);
         if (empty($id))
             return false;
-        
+
         if (!($user_id = $this->request->get('user_id', 'integer')))
             return false;
-        
+
         if (!($document = $this->documents->get_document($id)))
             return false;
 
         if ($user_id != $document->user_id)
             return false;
-        
+
         if (!($user = $this->users->get_user($document->user_id)))
             return false;
-        
-        if (!empty($document->params))
-        {
+
+        if (!empty($document->params)) {
             foreach ($document->params as $param_name => $param_value)
                 $this->design->assign($param_name, $param_value);
+
+            $cards = $this->cards->get_cards($document->user_id);
+            $active_card = '';
+            $short_active_card = '';
+
+            if (!empty($cards)) {
+                foreach ($cards as $card) {
+                    if($card->base_card == 1)
+                        $active_card = $card->pan;
+                }
+
+                if(!empty($active_card))
+                    $short_active_card = substr($active_card, -4);
+
+                $this->design->assign('short_active_card', $short_active_card);
+                $this->design->assign('active_card', $active_card);
+            }
+
+            $regaddress = $this->Addresses->get_address($user->regaddress_id);
+            $regaddress_full = $regaddress->adressfull;
+
+            $this->design->assign('regaddress_full', $regaddress_full);
+
+            $order = $this->orders->get_order($document->params['number']);
+            $contract = $this->contracts->get_contract($order->contract_id);
+            $this->design->assign('contract', $contract);
         }
 
-        if ($document->type == 'IND_USLOVIYA_NL')
-        {
+        if ($document->type == 'IND_USLOVIYA_NL') {
             $tribunal = $this->tribunals->find_tribunal($user->Regregion);
             $this->design->assign('tribunal', $tribunal);
         }
-        
-       	$tpl = $this->design->fetch('pdf/'.$document->template);
-        
+
+        $tpl = $this->design->fetch('pdf/' . $document->template);
+
         $this->pdf->create($tpl, $document->name, $document->template);
 
     }
-    
+
     private function action_preview()
     {
         $type = $this->request->get('type');
         $type = strtoupper(str_replace('.pdf', '', $type));
         if (empty($type))
             return false;
-        
+
         if (!($template = $this->documents->get_template($type)))
             return false;
-            
+
         if (!($template_name = $this->documents->get_template_name($type)))
             return false;
-            
+
         if (!($contract_id = $this->request->get('contract_id', 'integer')))
             return false;
-        
+
         if (!($contract = $this->contracts->get_contract($contract_id)))
             return false;
-        
+
 
         $ob_date = new DateTime();
-        $ob_date->add(DateInterval::createFromDateString($contract->period.' days'));
+        $ob_date->add(DateInterval::createFromDateString($contract->period . ' days'));
         $return_date = $ob_date->format('Y-m-d H:i:s');
 
         $return_amount = round($contract->amount + $contract->amount * $contract->base_percent * $contract->period / 100, 2);
@@ -105,27 +132,25 @@ class DocumentController extends Controller
             'passport_number' => substr(str_replace(array(' ', '-'), '', $contract_order->passport_serial), 4, 6),
             'asp' => $contract->accept_code,
         );
-        $regaddress_full = empty($contract_order->Regindex) ? '' : $contract_order->Regindex.', ';
-        $regaddress_full .= trim($contract_order->Regregion.' '.$contract_order->Regregion_shorttype);
-        $regaddress_full .= empty($contract_order->Regcity) ? '' : trim(', '.$contract_order->Regcity.' '.$contract_order->Regcity_shorttype);
-        $regaddress_full .= empty($contract_order->Regdistrict) ? '' : trim(', '.$contract_order->Regdistrict.' '.$contract_order->Regdistrict_shorttype);
-        $regaddress_full .= empty($contract_order->Reglocality) ? '' : trim(', '.$contract_order->Reglocality.' '.$contract_order->Reglocality_shorttype);
-        $regaddress_full .= empty($contract_order->Reghousing) ? '' : ', д.'.$contract_order->Reghousing;
-        $regaddress_full .= empty($contract_order->Regbuilding) ? '' : ', стр.'.$contract_order->Regbuilding;
-        $regaddress_full .= empty($contract_order->Regroom) ? '' : ', к.'.$contract_order->Regroom;
+        $regaddress_full = empty($contract_order->Regindex) ? '' : $contract_order->Regindex . ', ';
+        $regaddress_full .= trim($contract_order->Regregion . ' ' . $contract_order->Regregion_shorttype);
+        $regaddress_full .= empty($contract_order->Regcity) ? '' : trim(', ' . $contract_order->Regcity . ' ' . $contract_order->Regcity_shorttype);
+        $regaddress_full .= empty($contract_order->Regdistrict) ? '' : trim(', ' . $contract_order->Regdistrict . ' ' . $contract_order->Regdistrict_shorttype);
+        $regaddress_full .= empty($contract_order->Reglocality) ? '' : trim(', ' . $contract_order->Reglocality . ' ' . $contract_order->Reglocality_shorttype);
+        $regaddress_full .= empty($contract_order->Reghousing) ? '' : ', д.' . $contract_order->Reghousing;
+        $regaddress_full .= empty($contract_order->Regbuilding) ? '' : ', стр.' . $contract_order->Regbuilding;
+        $regaddress_full .= empty($contract_order->Regroom) ? '' : ', к.' . $contract_order->Regroom;
 
         $params['regaddress_full'] = $regaddress_full;
 
-        if ($type == 'IND_USLOVIYA_NL')
-        {
+        if ($type == 'IND_USLOVIYA_NL') {
             $params['tribunal'] = $this->tribunals->find_tribunal($contract_order->Regregion);
         }
-                
 
-        if ($type == 'POLIS_STRAHOVANIYA')
-        {
+
+        if ($type == 'POLIS_STRAHOVANIYA') {
             $insurance = new StdClass();
-            
+
             $insurance->create_date = date('Y-m-d H:i:s');
             $insurance->amount = round($contract->amount * $insurance_rate, 2);
             $insurance->start_date = date('Y-m-d 00:00:00', time() + (1 * 86400));
@@ -136,10 +161,10 @@ class DocumentController extends Controller
 
         foreach ($params as $param_name => $param_value)
             $this->design->assign($param_name, $param_value);
-        
-       	$tpl = $this->design->fetch('pdf/'.$template);
-        
+
+        $tpl = $this->design->fetch('pdf/' . $template);
+
         $this->pdf->create($tpl, $template_name, $template);
-        
+
     }
 }
